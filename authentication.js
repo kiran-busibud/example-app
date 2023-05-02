154a72fdfc669edffe1c0bf186c4d1b4e866ec49
kiran-busibud <kiran@zinosi.com>
2023-05-02 18:02:04 +0530

    HELP-11510 #time 2h session based authentication

--- authentication.js
+++ authentication.js
@@ -1,26 +1,52 @@
-module.exports = {
-    type: 'basic',
-    test: {
-      headers: {
-        'X-USERNAME': '{{bundle.authData.username}}',
-        'X-PASSWORD': '{{bundle.authData.password}}',
-      },
-      params: {
-        password: '{{bundle.authData.password}}',
-        username: '{{bundle.authData.username}}',
-      },
-      url: 'https://75c7-136-185-200-116.ngrok-free.app/zapier/auth',
+'use strict';
+
+const test = (z, bundle) =>
+  z.request({ url: 'https://auth-json-server.zapier-staging.com/me' });
+
+const getSessionKey = async (z, bundle) => {
+  const response = await z.request({
+    url: 'https://httpbin.zapier-tooling.com/post',
+    method: 'POST',
+    body: {
+      username: bundle.authData.username,
+      password: bundle.authData.password,
     },
+  });
+
+  return {
+    sessionKey: response.data.sessionKey,
+  };
+};
+
+const includeSessionKeyHeader = (request, z, bundle) => {
+  if (bundle.authData.sessionKey) {
+    request.headers = request.headers || {};
+    request.headers['X-API-Key'] = bundle.authData.sessionKey;
+  }
+
+  return request;
+};
+
+module.exports = {
+  config: {
+
+    type: 'session',
+    sessionConfig: { perform: getSessionKey },
+
     fields: [
-      { computed: false, key: 'username', required: true, label: 'username' },
+      { key: 'username', label: 'Username', required: true },
       {
-        computed: false,
         key: 'password',
+        label: 'Password',
         required: true,
-        label: 'password',
+
         type: 'password',
       },
     ],
-    basicConfig: {},
-  };
-  
+    test,
+
+    connectionLabel: '{{json.username}}',
+  },
+  befores: [includeSessionKeyHeader],
+  afters: [],
+};
